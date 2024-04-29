6b878270db673e9ab6f21743602c1c9015278b8f
Lavan Adicherla <143772951+lavankumara2015@users.noreply.github.com>
2024-04-29 17:05:23 +0530

    update

--- src/components/Usermessages/index.js
+++ src/components/Usermessages/index.js
@@ -1,102 +1,57 @@
 import React, { useEffect, useState } from "react";
 import "./userMessage.css";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Editor } from "primereact/editor";
-import EmojiPicker from "emoji-picker-react";
-import Picker from "emoji-picker-react";
+import  EmojiPicker from 'emoji-picker-react';
+import Picker from 'emoji-picker-react';
+
 
 function Index2(props) {
-  const {
-    message,
-    userData,
-    showMessage,
-    patientNumber,
-    setShowEditor,
-    setShowEmoji,
-    showEmoji,
-    showEditor,
-  } = props;
+  const { message, userData, showMessage,patientNumber, setShowEditor, setShowEmoji,showEmoji,showEditor } = props;
   const [textMessage, setTextMessage] = useState("");
-  const [reaction, setReaction] = useState("no");
-  const [text, setText] = useState("");
+  const [reaction , setReaction]=useState('no');
+  const [text, setText]=useState("");
   const [selectedMessageId, setSelectedMessageId] = useState(null);
-<<<<<<< HEAD
-=======
   
 
   // console.log(showMessage + "dataaa")
   
->>>>>>> 49c515fe705000f5f5f2d83e38844fc7455e3967
 
   const formatTimestamp = (timestamp) => {
     const date = new Date(parseInt(timestamp) * 1000);
     return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-<<<<<<< HEAD
-  };
-
-=======
   };  
->>>>>>> 49c515fe705000f5f5f2d83e38844fc7455e3967
   const helper = async (bodyData) => {
     try {
       const options = {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${process.env.token} `,
         },
         body: JSON.stringify(bodyData),
       };
-      const makeFetch = await fetch(
-        "https://node-ccoplnfjedo.onrender.com/message",
-        options
-      );
-      setTextMessage("");
+      const makeFetch = await fetch('http://localhost:3005/message', options);
+      setTextMessage("")
+    
     } catch (err) {
       console.log(err);
     }
   };
-<<<<<<< HEAD
-
-  const onSend = (e) => {
-=======
   const onSend =(e)=>{
->>>>>>> 49c515fe705000f5f5f2d83e38844fc7455e3967
     e.preventDefault();
     helper({
       messaging_product: "whatsapp",
       to: patientNumber,
       type: "text",
       data: {
-        text: textMessage,
-      },
-    });
-    setTextMessage("");
-  };
+          text: textMessage,
+    },
+  });
+  setTextMessage('');
+  }
   const handleReaction = (id) => {
-<<<<<<< HEAD
-    setSelectedMessageId(id === selectedMessageId ? null : id);
-    // console.log("selected message" + selectedMessageId)
-  };
-
-  const OnHandleReaction = (emoji, id) => {
-    const newReaction = reaction === emoji.emoji ? "" : emoji.emoji;
-    helper({
-      messaging_product: "whatsapp",
-      to: patientNumber,
-      type: "reaction",
-      data: {
-        message_id: id,
-        emoji: newReaction,
-      },
-    });
-    setReaction(newReaction);
-  };
-
-  const handleText = () => {
-    setShowEditor((prevState) => !prevState);
-=======
     setSelectedMessageId(id === selectedMessageId ? null : id); 
      // console.log("selected message" + selectedMessageId)
   }
@@ -116,59 +71,33 @@ function Index2(props) {
   }
     const handleText = () => {
     setShowEditor(prevState => !prevState); 
->>>>>>> 49c515fe705000f5f5f2d83e38844fc7455e3967
     setShowEmoji(false);
-    const messagesMainContainer = document.getElementById(
-      "messages-main-container-id"
-    );
+    const messagesMainContainer = document.getElementById("messages-main-container-id");
     messagesMainContainer.style.height = showEditor ? "76vh" : "40vh";
-<<<<<<< HEAD
-  };
-
-=======
   }
->>>>>>> 49c515fe705000f5f5f2d83e38844fc7455e3967
   const handleEmoji = () => {
-    setShowEmoji((prevState) => !prevState);
-    setShowEditor(false);
-    const messagesMainContainer = document.getElementById(
-      "messages-main-container-id"
-    );
+    setShowEmoji(prevState => !prevState);
+    setShowEditor(false); 
+    const messagesMainContainer = document.getElementById("messages-main-container-id");
     messagesMainContainer.style.height = showEmoji ? "76vh" : "37vh";
-  };
+  }
   const handleChange = (e) => {
     setTextMessage(e.target.value);
-  };
+  }
   const handleSubmit = (e) => {
-    e.preventDefault();
-    onSend(e);
-  };
+    e.preventDefault(); 
+    onSend(e); 
+  }
   const handleEmojiSelect = (emoji) => {
-<<<<<<< HEAD
-    setTextMessage(textMessage + emoji.emoji);
-    console.log("emoji selected" + emoji.emoji);
-  };
-
-  // const handleEmojiClick = (emoji,id) => {
-
-  //      console.log("handleEmojiClick" + emoji.emoji)
-  // }
-
-  return (
-=======
     setTextMessage(textMessage + emoji.emoji); 
     console.log("emoji selected" + emoji.emoji)
   }
   
-  const handleMedia =()=>{
-
-  }
   
 
  return (
->>>>>>> 49c515fe705000f5f5f2d83e38844fc7455e3967
     <>
-      {/* <ToastContainer
+    {/* <ToastContainer
     position="top-right"
           autoClose={1000}
           hideProgressBar={false}
@@ -180,79 +109,6 @@ function Index2(props) {
           pauseOnHover
           theme="dark"
     /> */}
-<<<<<<< HEAD
-      <div
-        className="messages-main-container-all"
-        id="messages-main-container-id">
-        {message &&
-          showMessage?.map((val, ind) => {
-            return (
-              <div key={ind} className="messages-main-container">
-                <div
-                  className={`${
-                    val.message_type === "Outgoing"
-                      ? "messages-main-container__text-left"
-                      : "messages-main-container__text-right"
-                  }`}>
-                  <div
-                    className="messages-main-container__text-inner"
-                    style={{
-                      backgroundColor: `${
-                        val.message_type === "Outgoing" ? "#D5FFD5" : "#FBFAFA"
-                      }`,
-                      alignSelf: `${
-                        val.message_type === "Outgoing"
-                          ? "flex-start"
-                          : "flex-end"
-                      }`,
-                      order: `${val.message_type === "Outgoing" ? "2" : "0"}`,
-                    }}>
-                    <h3 className="messages-main-container__h3">
-                      {val?.text?.body}
-                    </h3>
-                    <p className="messages-main-container__p">
-                      {formatTimestamp(val?.timestamp)}
-                    </p>
-                    {val?.type === "location" && (
-                      <div>
-                        <iframe
-                          className="location_container"
-                          src={`https://maps.google.com/maps?q=${
-                            val.location.latitude
-                          },${val.location.longitude}&z=${12}&output=embed`}
-                          loading="lazy"
-                          referrerPolicy="no-referrer-when-downgrade"
-                          title="google map"></iframe>
-                        <a
-                          className="location__link"
-                          href="https://www.google.com/maps/place/${val.location.latitude}N+${val.location.longitude}E">
-                          https://maps.google.com/maps
-                        </a>
-                      </div>
-                    )}
-                    {val?.type === "image" && val?.media_data?.image && (
-                      <div>
-                        <img
-                          src={`data:image/jpeg;base64,${val.media_data.image.toString(
-                            "base64"
-                          )}`}
-                          alt=""
-                          className="mediaImage"
-                        />
-                        <a
-                          href={`data:image/jpeg;base64,${val.media_data.image.toString(
-                            "base64"
-                          )}`}
-                          download="image.jpg">
-                          <img
-                            className="download__icon"
-                            src="assets/download.png"
-                            alt="download symbol"
-                          />
-                        </a>
-                      </div>
-                    )}
-=======
      <div className="messages-main-container-all" id="messages-main-container-id">
         {message && showMessage?.map((val, ind) => {
           return (
@@ -274,102 +130,28 @@ function Index2(props) {
             ></iframe>
             <a className="location__link" href="https://www.google.com/maps/place/${val.location.latitude}N+${val.location.longitude}E">https://maps.google.com/maps</a>
         </div>
->>>>>>> 49c515fe705000f5f5f2d83e38844fc7455e3967
 
-                    {val?.type === "video" && val?.media_data?.video && (
-                      <div>
-                        <video controls className="videoclass">
-                          <source
-                            src={`data:video/mp4;base64,${val.media_data.video.toString(
-                              "base64"
-                            )}`}
-                            type="video/mp4"
-                          />
-                        </video>
-                        <a
-                          href={`data:video/mp4;base64,${val.media_data.video.toString(
-                            "base64"
-                          )}`}
-                          download="video.mp4">
-                          <img
-                            className="download__icon"
-                            src="assets/download.png"
-                            alt="download symbol"
-                          />
-                        </a>
-                      </div>
-                    )}
+)}
+{val?.type === "image" && val?.media_data?.image && (
+  <div>
+    <img src={`data:image/jpeg;base64,${val.media_data.image.toString('base64')}`} alt="" className="mediaImage" />
+    <a href={`data:image/jpeg;base64,${val.media_data.image.toString('base64')}`} download="image.jpg">
+    <img className="download__icon" src="assets/download.png" alt="download symbol" />
+    </a>
+  </div>
+)}
 
-                    {val?.type === "document" && val?.media_data?.document && (
-                      <div className="mediaDocument">
-                        <iframe
-                          src={`data:application/pdf;base64,${val.media_data.document.toString(
-                            "base64"
-                          )}`}></iframe>
-                        <a
-                          href={`data:application/pdf;base64,${val.media_data.document.toString(
-                            "base64"
-                          )}`}
-                          download="document.pdf">
-                          <img
-                            className="download__icon"
-                            src="assets/download.png"
-                            alt="download symbol"
-                          />
-                        </a>
-                      </div>
-                    )}
+{val?.type === "video" && val?.media_data?.video && (
+  <div>
+    <video controls className="videoclass">
+      <source src={`data:video/mp4;base64,${val.media_data.video.toString('base64')}`} type="video/mp4" />
+    </video>
+    <a href={`data:video/mp4;base64,${val.media_data.video.toString('base64')}`} download="video.mp4">
+    <img className="download__icon" src="assets/download.png" alt="download symbol" />
+    </a>
+  </div>
+)}
 
-<<<<<<< HEAD
-                    {val?.type === "audio" && val?.media_data?.audio && (
-                      <div>
-                        <audio controls>
-                          <source
-                            src={`data:audio/mpeg;base64,${val.media_data.audio.toString(
-                              "base64"
-                            )}`}
-                            type="audio/mpeg"
-                          />
-                        </audio>
-                        <a
-                          href={`data:audio/mpeg;base64,${val.media_data.audio.toString(
-                            "base64"
-                          )}`}
-                          download="audio.mp3">
-                          <img
-                            className="download__icon"
-                            src="assets/download.png"
-                            alt="download symbol"
-                          />
-                        </a>
-                      </div>
-                    )}
-                  </div>
-                  <div className="messages-main-container-reaction-picker">
-                    <div>
-                      <img
-                        onClick={() => handleReaction(val?.id)}
-                        src="assets/emoji_btn2.webp"
-                        alt="emoji-logo"
-                        className="emoji_logo"
-                      />
-                      {selectedMessageId === val?.id && (
-                        <Picker
-                          reactionsDefaultOpen={true}
-                          onReactionClick={(emoji) =>
-                            OnHandleReaction(emoji, val?.id)
-                          }
-                          onClick={handleReaction}
-                          // onEmojiClick={handleEmojiClick}
-                        />
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-=======
 {val?.type === "document" && val?.media_data?.document && (
   <div className="mediaDocument">
     <iframe  src={`data:application/pdf;base64,${val.media_data.document.toString('base64')}`} ></iframe>
@@ -420,7 +202,6 @@ function Index2(props) {
           );
         })}
         
->>>>>>> 49c515fe705000f5f5f2d83e38844fc7455e3967
       </div>
 
       
