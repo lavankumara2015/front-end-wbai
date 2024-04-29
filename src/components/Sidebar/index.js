3310d2f8bbb964ac82d7b95f53e60351380de8a4
tejasshri <tejasshrishekhar08@gmail.com>
2024-04-29 16:53:18 +0530

    update

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,6 @@ import ReactContext from "../../context/ReactContext";
 import Index2 from "../Usermessages";
 import axios from "axios";
 
-
 export default function Sidebar() {
   const { users, setUsers } = useContext(ReactContext);
   const [isLoading, setIsLoading] = useState(false);
@@ -14,27 +13,39 @@ export default function Sidebar() {
   const [showMessage, setShowMessage] = useState([]);
   const [search, setSearch] = useState("");
   const [filteredUsers, setFilteredUsers] = useState([]);
-  const [patientNumber ,setpatientNumber]=useState("");
+  const [patientNumber, setpatientNumber] = useState("");
   const [showEditor, setShowEditor] = useState(false);
   const [showEmoji, setShowEmoji] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3005/messageData");
+        const response = await axios.get(
+          "https://node-ccoplnfjedo.onrender.com/messageData"
+        );
         setUserData(response?.data?.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
     fetchData();
   }, []);
 
   useEffect(() => {
+    if (users.length !== 0) {
+      console.log(users, "Users");
+      handleName(users[0]);
+      console.log(userData)
+    }
+  }, [userData]);
+
+  useEffect(() => {
     (async function () {
       try {
         setIsLoading(true);
-        const response = await fetch("http://localhost:3005/users");
+        const response = await fetch(
+          "https://node-ccoplnfjedo.onrender.com/users"
+        );
         if (response.ok) {
           const data = await response.json();
           setUsers(data.data);
@@ -47,8 +58,8 @@ export default function Sidebar() {
     })();
   }, []);
 
-
-  const handleName = (each, i) => {
+  const handleName = (each) => {
+    console.log(each, "Handle Name");
     setpatientNumber(each.patient_phone_number);
     const data = userData.filter((item) => {
       return item.from === each.patient_phone_number;
@@ -59,9 +70,6 @@ export default function Sidebar() {
     setShowEmoji(false);
   };
 
-
-
-
   useEffect(() => {
     const filteredData = users.filter((item) => {
       return item.name.toLowerCase().includes(search.toLowerCase());
@@ -69,10 +77,12 @@ export default function Sidebar() {
     setFilteredUsers(filteredData);
   }, [search, users]);
 
-
- 
-  
- 
+  // useEffect(() => {
+  //   if (filteredUsers.length !== 0) {
+  //     console.log("Called", filteredUsers);
+  //     handleName(filteredUsers[0]);
+  //   }
+  // }, [filteredUsers]);
 
   return (
     <>
@@ -80,7 +90,11 @@ export default function Sidebar() {
         <aside>
           <div className={styles.sidebarHeader}>
             <div className={styles.searchBox}>
-              <img src="assets/Search.png" alt="search-icon" className={styles.searchIcon} />
+              <img
+                src="assets/Search.png"
+                alt="search-icon"
+                className={styles.searchIcon}
+              />
               <input
                 type="search"
                 placeholder="Search for a Chat or User"
@@ -89,7 +103,11 @@ export default function Sidebar() {
               />
             </div>
             <div>
-              <img src="assets/filter.png" alt="filter-icon" className={styles.filterIcon} />
+              <img
+                src="assets/filter.png"
+                alt="filter-icon"
+                className={styles.filterIcon}
+              />
             </div>
           </div>
           <hr className={styles.hrLine} />
@@ -98,7 +116,7 @@ export default function Sidebar() {
               ? null
               : filteredUsers.map((each, i) => (
                   <li key={i}>
-                    <img src={each.image_url} alt="profile-photo"/>
+                    <img src={each.image_url} alt="profile-photo" />
                     <div onClick={(e) => handleName(each, i)}>
                       <h1>{each.name}</h1>
                       <p>{each.patient_phone_number}</p>
@@ -107,23 +125,20 @@ export default function Sidebar() {
                 ))}
           </ul>
         </aside>
-        
+
         <div className={styles.message_container}>
-        <Index2
+          <Index2
             message={message}
             userData={userData}
             showMessage={showMessage}
             patientNumber={patientNumber}
-            showEditor={showEditor} 
-           setShowEditor={setShowEditor} 
-            showEmoji={showEmoji} 
-           setShowEmoji={setShowEmoji} 
+            showEditor={showEditor}
+            setShowEditor={setShowEditor}
+            showEmoji={showEmoji}
+            setShowEmoji={setShowEmoji}
           />
-        
         </div>
-       
-        </div>
-    
+      </div>
     </>
   );
 }
